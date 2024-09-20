89d74c2eb72c32a169de20f9bafe8f33e066ab86
lifuaini2019 <lifuaini2019@gmail.com>
2024-09-20 20:50:05 +0800

    updated20240920_xk

---
+++ source/home/index.md
@@ -0,0 +1,9 @@
+---
+title: 首页
+date: 2023-09-23 10:00:00
+categories: "Home"
+permalink: index.html
+---
+
+
+123345647
---
+++ 只更新本地.sh
@@ -0,0 +1,12 @@
+#清除缓存文件 (db.json) 和已生成的静态文件 (public)。
+hexo clean
+
+# 命令用于生成静态文件，一般可以简写为 hexo g
+hexo generate
+
+#hexo server 命令用于启动本地服务器，一般可以简写为 hexo s
+
+#hexo deploy 命令用于部署网站，一般可以简写为 hexo d
+
+#hexo server 命令用于启动本地服务器，一般可以简写为 hexo s
+hexo server
